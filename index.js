bae0976d2f2031440e8fc1dac5260f938e81f6b8
LAtoyinWA2489 <88058990+LAtoyinWA2489@users.noreply.github.com>
2021-11-04 14:46:43 -0700

    All tasks complete

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ The function should:
 */
 
 function createMenuItem(name, price, category){
-    // const newItem = {
-    //   name,
-    //   price,
-    //   category,
-    // }
-    // return createMenuItem
+    const newItem = {
+      name,
+      price,
+      category,
+    }
+    return newItem;
 }
-// console.log('Task 1a', createMenuItem('taco', 8, 'Lunch'));
+console.log('Task 1a', createMenuItem('taco', 8, 'Lunch'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1b: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Invoke your function!
@@ -35,7 +35,9 @@ Test your createMenuItems function by doing the following:
   For example: createMenuItem("pizza",5,"lunch") would return this as the object: {name:"Pizza",price:5,category:"lunch"}
 */
 
-
+console.log('Task 1a', createMenuItem('sushi', 15, 'Lunch'));
+console.log('Task 1a', createMenuItem('cachupa', 8, 'Breakfast'));
+console.log('Task 1a', createMenuItem('sandwich', 8, 'Lunch'));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to the 
@@ -81,7 +83,8 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
-console.log(reviews[5].feedback);
+
+console.log('Task 3', reviews[5].feedback);
 
 
 
@@ -90,9 +93,9 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
+reviews[7].feedback = "this place is chill with really cool people, great for getting work done on weekdays";
 
-
-
+console.log(reviews[7]);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -104,9 +107,18 @@ Write a function that creates an object with name, rating, feedback, add the new
   4. should return the resulting array
 */
 
-function addReview(/*Your Code Here */){
-  /*Your Code Here */
+function addReview(array, name, rating, feedback){
+  const newReview = {
+    name,
+    rating,
+    feedback
+  }
+  array.push(newReview);
+  return array;
 }
+
+console.log('Task 5', addReview(reviews, 'Daniela', 5, 'Beautfilu atmosphere and wonderful vegan options!'));
+
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function to return a review based on the index of the review in the array.
@@ -119,11 +131,11 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(array, num){
+    return `${array[num].name} gave the restaurant a ${array[num].rating} star review, and their feedback was: ${array[num].feedback}`
 }
 
-
+console.log(getReviewByIndex(reviews, 0));
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -138,9 +150,10 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(array) {
+  return `${array[array.length - 1].name} gave the restaurant a ${array[array.length - 1].rating} star review, and their feedback was: ${array[array.length - 1].feedback}`
 } 
+console.log(getLastReview(reviews));
 
 
 
